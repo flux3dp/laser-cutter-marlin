a13f8feda45bdd6fac830d0e914c4969a2e7de45
yagami.cerberus <yagami-cerberus@users.noreply.github.com>
2017-07-12 18:15:38 +0800

    CHANGE BUFLEN

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -725,7 +725,7 @@
 
 // The ASCII buffer for serial input
 #define MAX_CMD_SIZE 96
-#define BUFSIZE 4
+#define BUFSIZE 16
 
 // Transfer Buffer Size
 // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
